ae3c9fa43d5b737d72f02d4372bc19df3f7a963b
Audrey_GALACHO <audrey.galacho@gmail.com>
2022-03-09 11:02:53 +0100

    fonction clean htlm

--- Site_recettes/blocs/functions.php
+++ Site_recettes/blocs/functions.php
@@ -13,53 +13,49 @@ function backButton($page) //Le bouton pour retourner à la page acceuil qui pou
 
 function displayRecipe(array $recipe): string // Fonction d'affichage des recettes EN DEUX MORCEAU
 {
-    $recipe_content = '';
-    $recipe_content .= '<h3 class= "card-title">' . $recipe['title'] . '</h3>';
+    $recipe_content = '<h3 class= "card-title">' . $recipe['title'] . '</h3>';
     $recipe_content .= '<p class="card-text">' . $recipe['recipe'] . '</p>';
     return $recipe_content;
 }
 
-function displayAuthor(string $authorEmail, array $users): string //fait suite a celle d'avant
-
+function displayAuthor(string $authorEmail, array $users): string //Affichage de l'auteur en nom complet
 {
     for ($i = 0; $i < count($users); $i++) {
         $author = $users[$i];
         if ($authorEmail === $author['email']) {
-            $authorCool = $author['full_name'] . ' (' . $author['age'] . ' ans)';
-            $recipeAuthor = '<i>' . $authorCool . '</i>';
+            $authorRecipe = $author['full_name'] . ' (' . $author['age'] . ' ans)';
+            $recipeAuthor = '<i>' . $authorRecipe . '</i>';
             return $recipeAuthor;
         }
     }
 }
 
-function dspAllRescipes(array $recipes , $users){
+function dspAllRescipes(array $recipes, $users)
+{
     include('../users/usersAll.php');
 
     foreach ($recipes as $recipe) {
         $dspRecipe = displayRecipe($recipe);
         $dspEndRecipeAuthor = displayAuthor($recipe['author'], $users);
-?>
+    ?>
         <article class="cadre">
             <p class="card-text"> <?php echo $dspRecipe . $dspEndRecipeAuthor;
-            if ($recipe['author'] === $_SESSION['userMail']) {
-            ?>
-                <form>
-                    <a href="recipes/update.php?id=<?php echo $recipe['recipe_id']; ?>">
-                        <input type="button" value="Modifier">
-                    </a>
-                    <a href="recipes/deleteRecipe.php?id=<?php echo $recipe['recipe_id']; ?>">
-                        <input type="button" value="Effacer">
-                    </a>
-                </form>
-            <?php
-            }
-            ?>
-            </p>
+                                    if ($recipe['author'] === $_SESSION['userMail']) {
+                                    ?>
+            <form>
+                <a href="recipes/update.php?id=<?php echo $recipe['recipe_id']; ?>">
+                    <input type="button" value="Modifier">
+                </a>
+                <a href="recipes/deleteRecipe.php?id=<?php echo $recipe['recipe_id']; ?>">
+                    <input type="button" value="Effacer">
+                </a>
+            </form>
+        <?php
+                                    }
+        ?>
+        </p>
         </article>
         </br>
-    <?php
+<?php
     }
-   
 }
-
-
