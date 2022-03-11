d6bf853678abc13f6fc9e8ea0820265f6b569e0e
Audrey <audrey.galacho@gmail.com>
2022-03-11 17:21:32 +0100

    ajout dossier app

---
+++ app/config.php
@@ -0,0 +1,10 @@
+<?php
+
+//!\/A GIT IGNORER /!\
+const DB_HOST = 'localhost';
+const DB_PORT = '3306';
+const DB_NAME = 'my_recipes';
+const USER_NAME = 'lupi';
+const USER_PASSWORD = 'yxpsgLifYi8LROY6';
+echo 'on est sur le fichier de config de la database c\'est OK';
+?>
