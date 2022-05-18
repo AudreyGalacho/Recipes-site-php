faf216fe984302c74b5cedf0aa226804a7552aab
Audrey <audrey.galacho@gmail.com>
2022-05-18 17:42:26 +0200

    Css/html zone de log modification

--- recipeSite/html/users/userLogIn.php
+++ recipeSite/html/users/userLogIn.php
@@ -1,14 +1,14 @@
 <aside class="logMessage">
-    <p>
-        Connexion :
-    </p>
-    <form method="POST" action="">
-        <label for="email">Email</label>
+    <h5>
+        Se connecter :
+    </h5>
+    <form class="log" method="POST" action="">
+        <label for="email">Email : </label>
         <input type="email" placeholder="your@email.com" name="email" required="required">
 
-        <label for="password">Password</label>
-        <input type="password" placeholder="password" name="password" required="required">
+        <label for="password">Password : </label>
+        <input type="password" placeholder="*********" name="password" required="required">
 
-        <input type="submit" value="envoyer">
+        <input type="submit" value="Connexion">
     </form>
 </aside>
