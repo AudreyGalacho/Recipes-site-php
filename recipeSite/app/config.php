38cf1b79f016ccf60d6b258316c16c40ffe050d7
Audrey <audrey.galacho@gmail.com>
2022-05-20 11:03:26 +0200

    fix (config)  : git ignore config.php

--- recipeSite/app/config.php
+++ recipeSite/app/config.php
@@ -1,7 +1,7 @@
 <?php
 //!\/A GIT IGNORER /!\
 const DB_HOST = 'localhost';
-const DB_PORT = '8889';
+const DB_PORT = '3306';
 const DB_NAME = 'my_recipes';
 const DB_CHARSET = 'UTF8';
 const USER_NAME = 'lupi';
