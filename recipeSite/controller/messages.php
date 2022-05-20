45fd2af48f28c2287d13cb2a25994001722d315f
Audrey_GALACHO <audrey.galacho@gmail.com>
2022-05-20 10:31:05 +0200

    refactor (messagelog) : Commentaires d'une portion de la fonction messageLog

--- recipeSite/controller/messages.php
+++ recipeSite/controller/messages.php
@@ -2,12 +2,12 @@
 function messageLog()
 
 {
-    if (isset($_SESSION['userLogged'])){
+   /* if (isset($_SESSION['userLogged'])){
         $messageWelcome = '<aside class="logMessage"><p> Bonjour ' . $_SESSION['userLogged'] . ' bienvenu !!!</p>';
         echo $messageWelcome;
         echo logOutButton() . '</aside>';
         return;
-    }
+   }*/
     // echo 'on est dans l\'affichage du log';
     isUserLogged(); //on exe pour savoir s'il y a eu une entree au log
     // var_dump(isUserLogged());
